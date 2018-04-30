039a10abf93811da48f7488a936632ea7d812cca
Badder Marzocchi <badder.marzocchi@cern.ch>
2018-04-30 17:58:04 +0200

    94X_data2017

--- MicroAODAnalysis/python/FlashGXAnalysis_cfi.py
+++ MicroAODAnalysis/python/FlashGXAnalysis_cfi.py
@@ -31,28 +31,21 @@ flashgxanalysis = cms.EDAnalyzer("FlashGXAnalysis",
 
     #triggerPaths                      = cms.vstring("NONE"), #No trigger studies
     #triggerPaths                      = cms.vstring("ALL"), #Make trigger studies
-    triggerPaths                      = cms.vstring("HLT_Photon20_CaloIdVL_IsoL",
-                                                    "HLT_Photon22_R9Id90_HE10_Iso40_EBOnly_PFMET40",
-                                                    "HLT_Photon22_R9Id90_HE10_Iso40_EBOnly_VBF",
-                                                    "HLT_Photon36_R9Id90_HE10_Iso40_EBOnly_PFMET40",
-                                                    "HLT_Photon36_R9Id90_HE10_Iso40_EBOnly_VBF",
-                                                    "HLT_Photon50_R9Id90_HE10_Iso40_EBOnly_PFMET40",
-                                                    "HLT_Photon50_R9Id90_HE10_Iso40_EBOnly_VBF",
-                                                    "HLT_Photon75_R9Id90_HE10_Iso40_EBOnly_PFMET40",
-                                                    "HLT_Photon75_R9Id90_HE10_Iso40_EBOnly_VBF",
-                                                    "HLT_Photon90_R9Id90_HE10_Iso40_EBOnly_PFMET40",
-                                                    "HLT_Photon90_R9Id90_HE10_Iso40_EBOnly_VBF",
-                                                    "HLT_Photon120_R9Id90_HE10_Iso40_EBOnly_PFMET40",
-                                                    "HLT_Photon120_R9Id90_HE10_Iso40_EBOnly_VBF"
-                                                   #"HLT_Photon22",
-                                                   #"HLT_Photon30",
-                                                   #"HLT_Photon36",
-                                                   #"HLT_Photon50",
-                                                   #"HLT_Photon75",
-                                                   #"HLT_Photon90",
-                                                   #"HLT_Photon120",
-                                                   #"HLT_Photon175",
-                                                   #"HLT_Photon165_HE10"
+    triggerPaths                      = cms.vstring("HLT_Photon60_R9Id90_CaloIdL_IsoL",
+                                                    "HLT_Photon20_HoverELoose",
+                                                    "HLT_Photon30_HoverELoose",
+                                                    "HLT_Photon40_HoverELoose",
+                                                    "HLT_Photon50_HoverELoose",
+                                                    "HLT_Photon60_HoverELoose",
+                                                    "HLT_Photon50_R9Id90_HE10_IsoM_EBOnly_PFJetsMJJ300DEta3_PFMET50",
+                                                    "HLT_Photon75_R9Id90_HE10_IsoM_EBOnly_PFJetsMJJ300DEta3",
+                                                    "HLT_Photon75_R9Id90_HE10_IsoM_EBOnly_PFJetsMJJ600DEta3"
+                                                    "HLT_DiJet110_35_Mjj650_PFMET110",
+                                                    "HLT_DiJet110_35_Mjj650_PFMET120",
+                                                    "HLT_DiJet110_35_Mjj650_PFMET130",
+                                                    "HLT_TripleJet110_35_35_Mjj650_PFMET110",
+                                                    "HLT_TripleJet110_35_35_Mjj650_PFMET120",
+                                                    "HLT_TripleJet110_35_35_Mjj650_PFMET130"
                                                    ), 
 
     photonPtThres                     = cms.double(20.),
@@ -63,22 +56,22 @@ flashgxanalysis = cms.EDAnalyzer("FlashGXAnalysis",
     applyHggPhotonID                  = cms.bool(False),
 
     #80X MVAphotonID 
-    phoMvaThres                       = cms.vdouble(0.2,0.2), #tight: EB,EE
+    #phoMvaThres                       = cms.vdouble(0.2,0.2), #tight: EB,EE
     #phoMvaThres                       = cms.vdouble(0.68,0.60), #loose: EB,EE
-    passElectronVetoThres             = cms.vdouble(0.5,0.5), #EB,EE
+    #passElectronVetoThres             = cms.vdouble(0.5,0.5), #EB,EE
     
     #92X MVAphotonID 
-    #phoMvaThres                       = cms.vdouble(0.27,0.14), #tight: EB,EE
+    phoMvaThres                       = cms.vdouble(0.27,0.14), #tight: EB,EE
     #phoMvaThres                       = cms.vdouble(0.67,0.54), #loose: EB,EE
-    #passElectronVetoThres             = cms.vdouble(0.5,0.5), #EB,EE
+    passElectronVetoThres             = cms.vdouble(0.5,0.5), #EB,EE
 
     #isoEffArea 80X
-    isoCorrEtaBinsCh                  = cms.vdouble(0.0000,1.0000,1.4790,2.0000,2.2000,2.3000,2.4000,999.),  
-    isoCorrAreaValCh                  = cms.vdouble(0.0360,0.0377,0.0306,0.0283,0.0254,0.0217,0.0167),
-    isoCorrEtaBinsNh                  = cms.vdouble(0.0000,1.0000,1.4790,2.0000,2.2000,2.3000,2.4000,999.),  
-    isoCorrAreaValNh                  = cms.vdouble(0.0597,0.0807,0.0629,0.0197,0.0184,0.0284,0.0591),
-    isoCorrEtaBinsPh                  = cms.vdouble(0.0000,1.0000,1.4790,2.0000,2.2000,2.3000,2.4000,999.),  
-    isoCorrAreaValPh                  = cms.vdouble(0.1210,0.1107,0.0699,0.1056,0.1457,0.1719,0.1998),
+    #isoCorrEtaBinsCh                  = cms.vdouble(0.0000,1.0000,1.4790,2.0000,2.2000,2.3000,2.4000,999.),  
+    #isoCorrAreaValCh                  = cms.vdouble(0.0360,0.0377,0.0306,0.0283,0.0254,0.0217,0.0167),
+    #isoCorrEtaBinsNh                  = cms.vdouble(0.0000,1.0000,1.4790,2.0000,2.2000,2.3000,2.4000,999.),  
+    #isoCorrAreaValNh                  = cms.vdouble(0.0597,0.0807,0.0629,0.0197,0.0184,0.0284,0.0591),
+    #isoCorrEtaBinsPh                  = cms.vdouble(0.0000,1.0000,1.4790,2.0000,2.2000,2.3000,2.4000,999.),  
+    #isoCorrAreaValPh                  = cms.vdouble(0.1210,0.1107,0.0699,0.1056,0.1457,0.1719,0.1998),
      
     #Tight 80X CutBased photonID 
     #hadronicOverEmThres               = cms.vdouble(0.02690,0.02130), #EB,EE
@@ -90,13 +83,13 @@ flashgxanalysis = cms.EDAnalyzer("FlashGXAnalysis",
     #egPhotonIsoEEThres                = cms.vdouble(2.61700,0.00340), #p0,p1
 
     #Medium 80X CutBased photonID 
-    hadronicOverEmThres               = cms.vdouble(0.03960,0.02190), #EB,EE
-    sigmaIetaIetaThres                = cms.vdouble(0.01022,0.03001), #EB,EE
-    egChargedHadronIsoThres           = cms.vdouble(0.44100,0.44200), #EB,EE
-    egNeutralHadronIsoEBThres         = cms.vdouble(2.72500,0.01480,0.000017), #p0,p1,p2
-    egNeutralHadronIsoEEThres         = cms.vdouble(1.71500,0.01630,0.000014), #p0,p1,p2
-    egPhotonIsoEBThres                = cms.vdouble(2.57100,0.00470), #p0,p1
-    egPhotonIsoEEThres                = cms.vdouble(3.86300,0.00340), #p0,p1
+    #hadronicOverEmThres               = cms.vdouble(0.03960,0.02190), #EB,EE
+    #sigmaIetaIetaThres                = cms.vdouble(0.01022,0.03001), #EB,EE
+    #egChargedHadronIsoThres           = cms.vdouble(0.44100,0.44200), #EB,EE
+    #egNeutralHadronIsoEBThres         = cms.vdouble(2.72500,0.01480,0.000017), #p0,p1,p2
+    #egNeutralHadronIsoEEThres         = cms.vdouble(1.71500,0.01630,0.000014), #p0,p1,p2
+    #egPhotonIsoEBThres                = cms.vdouble(2.57100,0.00470), #p0,p1
+    #egPhotonIsoEEThres                = cms.vdouble(3.86300,0.00340), #p0,p1
 
     #Loose 80X CutBased photonID 
     #hadronicOverEmThres               = cms.vdouble(0.05970,0.04810), #EB,EE
@@ -108,12 +101,12 @@ flashgxanalysis = cms.EDAnalyzer("FlashGXAnalysis",
     #egPhotonIsoEEThres                = cms.vdouble(6.64100,0.00340), #p0,p1
 
     #isoEffArea 92X
-    #isoCorrEtaBinsCh                  = cms.vdouble(0.0000,1.0000,1.4790,2.0000,2.2000,2.3000,2.4000,999.),  
-    #isoCorrAreaValCh                  = cms.vdouble(0.0385,0.0468,0.0435,0.0378,0.0338,0.0314,0.0269),
-    #isoCorrEtaBinsNh                  = cms.vdouble(0.0000,1.0000,1.4790,2.0000,2.2000,2.3000,2.4000,999.),  
-    #isoCorrAreaValNh                  = cms.vdouble(0.0636,0.1103,0.0759,0.0236,0.0151,0.00007,0.0132),
-    #isoCorrEtaBinsPh                  = cms.vdouble(0.0000,1.0000,1.4790,2.0000,2.2000,2.3000,2.4000,999.),  
-    #isoCorrAreaValPh                  = cms.vdouble(0.1240,0.1093,0.0631,0.0779,0.0999,0.1155,0.1373),
+    isoCorrEtaBinsCh                  = cms.vdouble(0.0000,1.0000,1.4790,2.0000,2.2000,2.3000,2.4000,999.),  
+    isoCorrAreaValCh                  = cms.vdouble(0.0385,0.0468,0.0435,0.0378,0.0338,0.0314,0.0269),
+    isoCorrEtaBinsNh                  = cms.vdouble(0.0000,1.0000,1.4790,2.0000,2.2000,2.3000,2.4000,999.),  
+    isoCorrAreaValNh                  = cms.vdouble(0.0636,0.1103,0.0759,0.0236,0.0151,0.00007,0.0132),
+    isoCorrEtaBinsPh                  = cms.vdouble(0.0000,1.0000,1.4790,2.0000,2.2000,2.3000,2.4000,999.),  
+    isoCorrAreaValPh                  = cms.vdouble(0.1240,0.1093,0.0631,0.0779,0.0999,0.1155,0.1373),
 
     #Tight 92X CutBased photonID 
     #hadronicOverEmThres               = cms.vdouble(0.0200,0.0250), #EB,EE
@@ -125,13 +118,13 @@ flashgxanalysis = cms.EDAnalyzer("FlashGXAnalysis",
     #egPhotonIsoEEThres                = cms.vdouble(3.2720,0.0040), #p0,p1
 
     #Medium 92X CutBased photonID 
-    #hadronicOverEmThres               = cms.vdouble(0.0350,0.0270), #EB,EE
-    #sigmaIetaIetaThres                = cms.vdouble(0.0103,0.0271), #EB,EE
-    #egChargedHadronIsoThres           = cms.vdouble(1.4160,1.0120), #EB,EE
-    #egNeutralHadronIsoEBThres         = cms.vdouble(2.4910,0.0126,0.000026), #p0,p1,p2
-    #egNeutralHadronIsoEEThres         = cms.vdouble(9.1310,0.0119,0.000025), #p0,p1,p2
-    #egPhotonIsoEBThres                = cms.vdouble(2.9520,0.0040), #p0,p1
-    #egPhotonIsoEEThres                = cms.vdouble(4.0950,0.0040), #p0,p1
+    hadronicOverEmThres               = cms.vdouble(0.0350,0.0270), #EB,EE
+    sigmaIetaIetaThres                = cms.vdouble(0.0103,0.0271), #EB,EE
+    egChargedHadronIsoThres           = cms.vdouble(1.4160,1.0120), #EB,EE
+    egNeutralHadronIsoEBThres         = cms.vdouble(2.4910,0.0126,0.000026), #p0,p1,p2
+    egNeutralHadronIsoEEThres         = cms.vdouble(9.1310,0.0119,0.000025), #p0,p1,p2
+    egPhotonIsoEBThres                = cms.vdouble(2.9520,0.0040), #p0,p1
+    egPhotonIsoEEThres                = cms.vdouble(4.0950,0.0040), #p0,p1
 
     #Loose 92X CutBased photonID 
     #hadronicOverEmThres               = cms.vdouble(0.1050,0.0290), #EB,EE
@@ -199,18 +192,18 @@ flashgxanalysis = cms.EDAnalyzer("FlashGXAnalysis",
     #merge3rdJet                    = cms.bool(False),
     thirdJetDRCut                  = cms.double(0.8),
     pujidEtaBins                   = cms.vdouble(0.00,2.50,2.75,3.00,5.00),  
-    #pujidWpPtBin1                  = cms.vdouble(0.69, -0.35, -0.26, -0.21), # 80X tight
-    #pujidWpPtBin2                  = cms.vdouble(0.69, -0.35, -0.26, -0.21), # 80X tight
-    #pujidWpPtBin3                  = cms.vdouble(0.69, -0.35, -0.26, -0.21), # 80X tight
-    #pujidWpPtBin4                  = cms.vdouble(0.86, -0.10, -0.05, -0.01), # 80X tight
-    pujidWpPtBin1                  = cms.vdouble(0.18, -0.55, -0.42, -0.36), # 80X medium
-    pujidWpPtBin2                  = cms.vdouble(0.18, -0.55, -0.42, -0.36), # 80X medium
-    pujidWpPtBin3                  = cms.vdouble(0.18, -0.55, -0.42, -0.36), # 80X medium
-    pujidWpPtBin4                  = cms.vdouble(0.61, -0.35, -0.23, -0.17), # 80X medium
-    #pujidWpPtBin1                  = cms.vdouble(-0.97, -0.68, -0.53, -0.47), # 80X loose
-    #pujidWpPtBin2                  = cms.vdouble(-0.97, -0.68, -0.53, -0.47), # 80X loose
-    #pujidWpPtBin3                  = cms.vdouble(-0.97, -0.68, -0.53, -0.47), # 80X loose
-    #pujidWpPtBin4                  = cms.vdouble(-0.89, -0.52, -0.38, -0.30), # 80X loose
+    #pujidWpPtBin1                  = cms.vdouble(0.69, -0.35, -0.26, -0.21), # 94X tight
+    #pujidWpPtBin2                  = cms.vdouble(0.69, -0.35, -0.26, -0.21), # 94X tight
+    #pujidWpPtBin3                  = cms.vdouble(0.69, -0.35, -0.26, -0.21), # 94X tight
+    #pujidWpPtBin4                  = cms.vdouble(0.86, -0.10, -0.05, -0.01), # 94X tight
+    pujidWpPtBin1                  = cms.vdouble(0.18, -0.55, -0.42, -0.36), # 94X medium
+    pujidWpPtBin2                  = cms.vdouble(0.18, -0.55, -0.42, -0.36), # 94X medium
+    pujidWpPtBin3                  = cms.vdouble(0.18, -0.55, -0.42, -0.36), # 94X medium
+    pujidWpPtBin4                  = cms.vdouble(0.61, -0.35, -0.23, -0.17), # 94X medium
+    #pujidWpPtBin1                  = cms.vdouble(-0.97, -0.68, -0.53, -0.47), # 94X loose
+    #pujidWpPtBin2                  = cms.vdouble(-0.97, -0.68, -0.53, -0.47), # 94X loose
+    #pujidWpPtBin3                  = cms.vdouble(-0.97, -0.68, -0.53, -0.47), # 94X loose
+    #pujidWpPtBin4                  = cms.vdouble(-0.89, -0.52, -0.38, -0.30), # 94X loose
     rmsforwardCut                  = cms.double(3.0), # default was 0.03 , running on loose pujid
     
     deltaEtaJets                   = cms.double(1.),
